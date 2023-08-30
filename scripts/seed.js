3b1adaae4226fd1784012c166ce9c5a1cdc7d59b
Jack Ball <ball.jack2376@gmail.com>
2023-08-30 16:38:16 -0600

    adds movies to db inefficiently

---
+++ scripts/seed.js
@@ -0,0 +1,25 @@
+import { Movie, Rating, User, db } from "../src/model.js";
+import movieData from "./data/movies.json" assert { type: "json" };
+
+console.log("Syncing database.....");
+await db.sync({ force: true });
+
+console.log("Seeding database...");
+
+const moviesInDB = movieData.map(async (movie) => {
+  const releaseDate = new Date(Date.parse(movie.releaseDate));
+  const { title, overview, posterPath } = movie;
+
+  const newMovie = await Movie.create({
+    title,
+    overview,
+    posterPath,
+    releaseDate,
+  });
+
+  return newMovie;
+});
+
+await Promise.all(moviesInDB);
+
+console.log(moviesInDB);
